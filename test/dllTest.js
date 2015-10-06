9af43519ce23b6efe00b29e873f8f0c9797eea68
Matthew Yang <yang70@gmail.com>
2015-10-06 15:53:23 -0700

    passing tests

--- test/dllTest.js
+++ test/dllTest.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
-var Dll    = require('../lib/dll');
+var Dll    = require('../lib/dll').Dll;
+var Node   = require('../lib/dll').Node;
 
 describe('create doubly linked list and make sure it functions', function(){
   it('head should be null, then set to a node', function(){
@@ -7,34 +8,28 @@ describe('create doubly linked list and make sure it functions', function(){
     expect(list.head).to.equal(null);
     var newNode = new Node(5);
     list.insert(newNode);
-    expect(list.head).to.equal(5);
+    expect(list.head.value).to.equal(5);
   })
 
   it('should search and remove a node, null if not there', function(){
     var list = new Dll;
-    var values = [2, 4, 1, 5, 6];
-    values.forEach(function(){
-      if(element == 4){
-        testNode = new Node(4);
-        list.insert(testNode);
-      } else {
-        list.insert(new Node(element));
-      }
+    var values = [2, 4, 1, 5, 6, 9]
+    values.forEach(function(element){
+      list.insert(new Node(element));
     })
-
-    expect(list.head).to.equal(6);
-    expect(list.search(4)).to.equal(testNode);
-    expect(list.remove(6)).to.equal(6);
-    expect(list.head).to.equal(5);
-    expect(list.remove(6)).to.equal(null);
+    expect(list.head.value).to.equal(9);
+    expect(list.search(1).value).to.equal(1);
+    expect(list.remove(5)).to.equal(5);
+    expect(list.head.value).to.equal(9);
+    expect(list.remove(5)).to.equal(null);
   })
 
   it('should print out comma-separated list of values', function(){
     var list = new Dll;
     var values = [2, 4, 1, 5, 6];
-    values.forEach(function(){
-        list.insert(new Node(element));
-    })
-    expect(list.toString()).to.equal('2, 4, 1, 5, 6');
+    values.forEach(function(element){
+      list.insert(new Node(element));
+    });
+    expect(list.toString()).to.equal('6, 5, 1, 4, 2');
   })
 })
